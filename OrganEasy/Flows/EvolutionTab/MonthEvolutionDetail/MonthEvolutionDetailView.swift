58b15a7f3e1e5d159c72812d85d5bf4c0cc8376f
Walter Júnior <waltervreis@icloud.com>
2025-09-02 12:41:16 -0300

    Fixed edit evolution

--- OrganEasy/Flows/EvolutionTab/MonthEvolutionDetail/MonthEvolutionDetailView.swift
+++ OrganEasy/Flows/EvolutionTab/MonthEvolutionDetail/MonthEvolutionDetailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct MonthEvolutionDetailView: View {
     
     @EnvironmentObject var provider: RepositoryProvider
+    @EnvironmentObject var navManager: EvolutionNavigationManager
     @StateObject var viewModel: MonthEvolutionDetailViewModel = MonthEvolutionDetailViewModel()
     
     let month: String
@@ -31,7 +32,7 @@ struct MonthEvolutionDetailView: View {
                         Text(evolution.value.toBRL())
                     }
                     .onTapGesture {
-                        viewModel.edit(with: evolution)
+                        navManager.path.append(EvolutionRouter.evolution(evolution.id))
                     }
                     .swipeActions(edge: .trailing, allowsFullSwipe: true) {
                         Button(role: .destructive) {
